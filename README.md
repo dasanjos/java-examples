8f025b322ed5e16be78518e0850412e40c667512
Daniel Anjos <daniel@rubikssolutions.com>
2013-10-31 20:54:59 +0200

    Adding answer to amazon coding question

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Java implementation of several puzzles and programming test tasks (with JUnit te
 List of Examples:
 -----------------
 
+* Company - Who's Our Boss (Amazon 2013)
 * Decathlon - 
 * Look Ahead Iterator - 
 * Fibbonaci (Numbers, Sum, Average) - http://en.wikipedia.org/wiki/Fibonacci_number 
@@ -19,3 +20,30 @@ TODO:
 * Merge Generic and Optimized Zebra Puzzle Algorithms
 * move input.csv and template.xsl from resources/ to resources/zebraPuzzle
 * Read: http://java.sun.com/developer/technicalArticles/Interviews/community/pepperdine_qa.html
+
+
+### Amazon 2013 Test Task - Who's Our Boss
+
+Initech is a company which has CEO Bill and a hierarchy of employees. 
+Employees can have a list of other employees reporting to them, which can themselves have reports, and so on. 
+An employee with at least one report is called a manager.  
+
+Please implement the closestCommonManager method to find the closest manager (i.e. farthest from the CEO) to two employees. 
+You may assume that all employees eventually report up to the CEO.   
+
+**Sample Data:**
+* CEO Bill has 3 employees reporting to him: {Dom, Samir, Michael}  
+* Dom has three reports { Peter, Bob, Porter}  
+* Samir has no reports {}  
+* Michael has no reports {}  
+* Peter has 2 reports {Milton, Nina}  
+* Bob has no reports {}  
+* Porter has no reports {}  
+* Milton has no reports {}  
+* Nina has no reports {}  
+
+**Sample calls:**
+* closestCommonManager(Milton, Nina) = Peter  
+* closestCommonManager(Nina, Porter) = Dom  
+* closestCommonManager(Nina, Samir) = Bill  
+* closestCommonManager(Peter, Nina) = Peter
---
+++ src/test/java/com/dasanjos/java/CompanyTest.java
@@ -0,0 +1,65 @@
+package com.dasanjos.java;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.dasanjos.java.Company.Employee;
+
+public class CompanyTest {
+
+	Employee bill = new Employee(1, "Bill");
+	Employee dom = new Employee(2, "Dom");
+	Employee samir = new Employee(3, "Samir");
+	Employee michael = new Employee(4, "Michael");
+	Employee bob = new Employee(5, "Bob");
+	Employee peter = new Employee(6, "Peter");
+	Employee porter = new Employee(7, "Porter");
+	Employee milton = new Employee(8, "Milton");
+	Employee nina = new Employee(9, "Nina");
+
+	
+	@Before
+	public void setup(){
+		bill.addReport(dom);
+		bill.addReport(samir);
+		bill.addReport(michael);
+
+		dom.addReport(bob);
+		dom.addReport(peter);
+		dom.addReport(porter);
+				
+		peter.addReport(milton);
+		peter.addReport(nina);
+	}
+	
+	@Test
+	public void testFirstLevel() {
+		Assert.assertEquals(bill.getId(), Company.closestCommonManager(bill, dom, samir).getId());
+	}
+
+	@Test
+	public void testSecondLevel() {
+		Assert.assertEquals(dom.getId(), Company.closestCommonManager(bill, peter, porter).getId());
+	}
+
+	@Test
+	public void testThirdLevel() {
+		Assert.assertEquals(peter.getId(), Company.closestCommonManager(bill, milton, nina).getId());
+	}
+
+	@Test
+	public void testCrossOneLevel() {
+		Assert.assertEquals(dom.getId(), Company.closestCommonManager(bill, nina, porter).getId());
+	}
+
+	@Test
+	public void testCrossTwoLevels() {
+		Assert.assertEquals(bill.getId(), Company.closestCommonManager(bill, nina, samir).getId());
+	}
+
+	@Test
+	public void testSameHierarchy() {
+		Assert.assertEquals(peter.getId(), Company.closestCommonManager(bill, peter, nina).getId());
+	}
+}
